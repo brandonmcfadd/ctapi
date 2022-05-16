b1c2361fd06a36f742c1113946d7e9d1382223e7
brandon <brandon@brandonmcfadden.com>
2022-05-16 16:56:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ TRAIN_STATION_STOP_IDS = "30197,30198"
 
 # Enter the bus stop #'s for the stops you want estimated times for
 ENABLE_BUS_TRACKER = True  # Enter True or False to Enable or Disable Bus Portion
-BUS_STOP_STOP_IDS = "5465,1323"
+BUS_STOP_STOP_IDS = "5465,1323,11264,18261"
 # Enter the corresponding bus route # you want for each bus_stop_id
-BUS_STOP_ROUTE_IDS = "76,74"
+BUS_STOP_ROUTE_IDS = "76,74,82,82"
 
 # Enter the Divvy station #'s to lookup
 ENABLE_DIVVY_STATION_CHECK = True  # Enter True or False to Enable or Disable Divvy Portion
@@ -113,7 +113,8 @@ def get_latest_cta_tweet():
     found_tweet = False
     # All status Tweets start with an Open Bracket - Hoping to filter out some of the other garbage
     while found_tweet is False:
-        found_tweet = str(tweets_json["data"][tweet_counter]["text"]).startswith('[')
+        found_tweet = str(
+            tweets_json["data"][tweet_counter]["text"]).startswith('[')
         latest_tweet = tweets_json["data"][tweet_counter]["text"]
         tweet_counter += 1
     return latest_tweet
@@ -147,7 +148,7 @@ def add_train_stop_to_json(eta, stop_id):
     station_name = eta.find('staNm').text
 
     stop_information["full_name"] = eta.find(
-        'rt').text + " Line towards " + eta.find('destNm').text
+        'rt').text + " Line to " + eta.find('destNm').text
     stop_information["destination_name"] = eta.find('destNm').text
     stop_information["route"] = eta.find('rt').text
     stop_information["estimated_times"] = []
@@ -157,11 +158,19 @@ def add_train_stop_to_json(eta, stop_id):
 
 def add_bus_stop_to_json(prd, stop_id):
     """Function is called if a new bus stop is identified per API Call"""
+    bus_to_replace = {
+        " St": '',
+        " Rd": "",
+        " Ave": "",
+        "Town Center": "Twn Ctr"
+    }
     stop_information = {}
-
+    for key, value in bus_to_replace.items():
+        destination_name = re.sub(r"\b" + key + r"\b", value,
+                                  prd.find('des').text)
     stop_information["full_name"] = prd.find(
-        'rt').text + " towards " + prd.find('des').text
-    stop_information["destination_name"] = prd.find('des').text
+        'rt').text + " to " + destination_name
+    stop_information["destination_name"] = destination_name
     stop_information["route"] = prd.find('rt').text
     stop_information["stop_name"] = prd.find('stpnm').text
     stop_information["estimated_times"] = []
@@ -196,7 +205,7 @@ def add_train_eta_to_array(eta, station_name, stop_id):
             arrival = eta.find('arrT').text
             estimated_time = str(minutes_between(prediction, arrival))
             (arrival_information["trains"][station_name][stop_id]
-            ["estimated_times"].append(estimated_time + "min+"))
+             ["estimated_times"].append(estimated_time + "min+"))
     else:
         if eta.find('isApp').text == "1":
             arrival_information["trains"][station_name][stop_id][
@@ -206,7 +215,7 @@ def add_train_eta_to_array(eta, station_name, stop_id):
             arrival = eta.find('arrT').text
             estimated_time = str(minutes_between(prediction, arrival))
             (arrival_information["trains"][station_name][stop_id]
-            ["estimated_times"].append(estimated_time + "min-"))
+             ["estimated_times"].append(estimated_time + "min-"))
 
 
 def bus_eta_times(bus_api_response):
@@ -235,7 +244,13 @@ def add_bus_eta_to_array(prd, stop_id):
 
 def divvy_process_station_stats(station_stats, station_information):
     """Takes Station Information and Stats from API Call and gets needed information"""
-    to_replace = {" St": '', " Rd": "", " Ave": ""}
+    divvy_to_replace = {
+        " St": '',
+        " Rd": "",
+        " Ave": "",
+        "Town": "Twn",
+        "Center": "Ctr"
+    }
     for station in station_information['data']['stations']:
         station_id = station['station_id']
         if station_id in DIVVY_STATION_IDS:
@@ -245,7 +260,7 @@ def divvy_process_station_stats(station_stats, station_information):
                 (station['lat'], station['lon'])).miles
             station_distance_short = str(round(station_distance_long,
                                                2)) + "mi"
-            for key, value in to_replace.items():
+            for key, value in divvy_to_replace.items():
                 station_name = re.sub(r"\b" + key + r"\b", value,
                                       station['name'])
             found_station_information["station_name"] = station_name
@@ -273,8 +288,9 @@ def create_string_of_items(items):
         if string_count == 0:
             string = item
             string_count += 1
-        elif string_count == 1:
+        elif string_count > 0 and string_count <3:
             string = string + ", " + item
+            string_count += 1
     return string
 
 
@@ -327,14 +343,14 @@ def information_output_to_display(arrival_information_input):
                 'item_type':
                 "bus"
             })
-        except: # pylint: disable=bare-except
+        except:  # pylint: disable=bare-except
             display_information_output.append({
                 'line_1':
                 arrival_information['buses'][bus]["stop_name"],
                 'line_2':
                 arrival_information['buses'][bus]["full_name"],
                 'line_3':
-                    "No arrivals found :(",
+                "No arrivals found :(",
                 'item_type':
                 "bus"
             })
@@ -367,10 +383,20 @@ def information_to_display(status):
     loop_count = 0
     while loop_count < len(status):
         image = Image.new('1', (epd.height, epd.width),
-                            255)  # 255: clear the frame
+                          255)  # 255: clear the frame
         draw = ImageDraw.Draw(image)
 
         try:
+            if status[loop_count]['item_type'] == "train":
+                icon_train_resized = icon_train.resize(corner_image_size)
+                image.paste(icon_train_resized, (225, 35))
+            elif status[loop_count]['item_type'] == "bus":
+                icon_bus_resized = icon_bus.resize(corner_image_size)
+                image.paste(icon_bus_resized, (225, 35))
+            elif status[loop_count]['item_type'] == "bicycle":
+                icon_bicycle_resized = icon_bicycle.resize(corner_image_size)
+                image.paste(icon_bicycle_resized, (225, 35))
+
             # Store & Draw the location 1
             item_1_line_1 = status[loop_count]['line_1']
             draw.text((1, 1), item_1_line_1, font=bold_font, fill=0)
@@ -383,15 +409,6 @@ def information_to_display(status):
             item_1_line_3 = status[loop_count]['line_3']
             draw.text((1, 38), item_1_line_3, font=standard_font, fill=0)
 
-            if status[loop_count]['item_type'] == "train":
-                icon_train_resized = icon_train.resize(corner_image_size)
-                image.paste(icon_train_resized, (225, 35))
-            elif status[loop_count]['item_type'] == "bus":
-                icon_bus_resized = icon_bus.resize(corner_image_size)
-                image.paste(icon_bus_resized, (225, 35))
-            elif status[loop_count]['item_type'] == "bicycle":
-                icon_bicycle_resized = icon_bicycle.resize(corner_image_size)
-                image.paste(icon_bicycle_resized, (225, 35))
         except:  # pylint: disable=bare-except
             item_1_line_1 = ""
             item_1_line_2 = ""
@@ -401,6 +418,16 @@ def information_to_display(status):
         draw.line((0, 61, 250, 61), fill=0, width=3)
 
         try:
+            if status[loop_count]['item_type'] == "train":
+                icon_train_resized = icon_train.resize(corner_image_size)
+                image.paste(icon_train_resized, (225, 97))
+            elif status[loop_count]['item_type'] == "bus":
+                icon_bus_resized = icon_bus.resize(corner_image_size)
+                image.paste(icon_bus_resized, (225, 97))
+            elif status[loop_count]['item_type'] == "bicycle":
+                icon_bicycle_resized = icon_bicycle.resize(corner_image_size)
+                image.paste(icon_bicycle_resized, (225, 97))
+
             # Store & Draw the location 1
             item_2_line_1 = status[loop_count]['line_1']
             draw.text((1, 65), item_2_line_1, font=bold_font, fill=0)
@@ -412,16 +439,6 @@ def information_to_display(status):
             # Store & Draw the ETA 1
             item_2_line_3 = status[loop_count]['line_3']
             draw.text((1, 102), item_2_line_3, font=standard_font, fill=0)
-
-            if status[loop_count]['item_type'] == "train":
-                icon_train_resized = icon_train.resize(corner_image_size)
-                image.paste(icon_train_resized, (225, 97))
-            elif status[loop_count]['item_type'] == "bus":
-                icon_bus_resized = icon_bus.resize(corner_image_size)
-                image.paste(icon_bus_resized, (225, 97))
-            elif status[loop_count]['item_type'] == "bicycle":
-                icon_bicycle_resized = icon_bicycle.resize(corner_image_size)
-                image.paste(icon_bicycle_resized, (225, 97))
         except:  # pylint: disable=bare-except
             item_2_line_1 = ""
             item_2_line_2 = ""
@@ -442,20 +459,24 @@ def information_to_display(status):
         epd.display(epd.getbuffer(image))
 
         # Wait a respectable amount of time so the display can refresh
-        print("Sleeping 5 Seconds")
-        time.sleep(5)
+        print("Sleeping 4 Seconds")
+        time.sleep(4)
 
 
 def tweet_to_display():
     """Used to output the latest CTA Tweet to the Display"""
     corner_image_size = (25, 25)
-    tweet_text = textwrap.wrap(get_latest_cta_tweet(), width=25)
+    tweet_text = re.sub(
+        r'http\S+|( |(?<![a-zA-Z]))[M][o][r][e][:]( |(?![a-zA-Z]))', '',
+        str(get_latest_cta_tweet()))
+    tweet_text_wrapped = textwrap.wrap(tweet_text, width=25)
     icon_twitter = Image.open("/home/pi/ctapi/icons/twitter.png")
-    tweet_length = len(tweet_text)
+    tweet_length = len(tweet_text_wrapped)
     current_tweet_page = 1
     total_tweet_pages = math.ceil(tweet_length / 4)
     printed_lines = 0
-    while printed_lines != len(tweet_text) and ENABLE_TWITTER_LOOKUP is True:
+    while printed_lines != len(
+            tweet_text_wrapped) and ENABLE_TWITTER_LOOKUP is True:
         twitter_image = Image.new('1', (epd.height, epd.width),
                                   255)  # 255: clear the frame
         twitter_draw = ImageDraw.Draw(twitter_image)
@@ -465,25 +486,25 @@ def tweet_to_display():
 
         # Store & Draw Tweet
         try:
-            tweet_line_2 = tweet_text[printed_lines]
+            tweet_line_2 = tweet_text_wrapped[printed_lines]
             printed_lines += 1
             twitter_draw.text((0, 20), tweet_line_2, font=tweet_font, fill=0)
         except:  # pylint: disable=bare-except
             tweet_line_2 = ""
         try:
-            tweet_line_3 = tweet_text[printed_lines]
+            tweet_line_3 = tweet_text_wrapped[printed_lines]
             printed_lines += 1
             twitter_draw.text((0, 40), tweet_line_3, font=tweet_font, fill=0)
         except:  # pylint: disable=bare-except
             tweet_line_3 = ""
         try:
-            tweet_line_4 = tweet_text[printed_lines]
+            tweet_line_4 = tweet_text_wrapped[printed_lines]
             printed_lines += 1
             twitter_draw.text((0, 60), tweet_line_4, font=tweet_font, fill=0)
         except:  # pylint: disable=bare-except
             tweet_line_4 = ""
         try:
-            tweet_line_5 = tweet_text[printed_lines]
+            tweet_line_5 = tweet_text_wrapped[printed_lines]
             printed_lines += 1
             twitter_draw.text((0, 80), tweet_line_5, font=tweet_font, fill=0)
         except:  # pylint: disable=bare-except
@@ -491,7 +512,8 @@ def tweet_to_display():
         icon_twitter = icon_twitter.resize(corner_image_size)
         twitter_image.paste(icon_twitter, (225, 97))
         try:
-            tweet_line_6 = "Page " + str(current_tweet_page) + " / " + str(total_tweet_pages)
+            tweet_line_6 = "Page " + str(current_tweet_page) + " / " + str(
+                total_tweet_pages)
             twitter_draw.text((0, 100), tweet_line_6, font=tweet_font, fill=0)
         except:  # pylint: disable=bare-except
             tweet_line_6 = ""
@@ -512,8 +534,8 @@ def tweet_to_display():
         epd.display(epd.getbuffer(twitter_image))
 
         # Wait a respectable amount of time so the display can refresh
-        print("Sleeping 5 Seconds")
-        time.sleep(5)
+        print("Sleeping 4 Seconds")
+        time.sleep(4)
 
 
 print("Welcome to TrainTracker, Python/RasPi Edition!")
@@ -547,7 +569,8 @@ while True:  # Where the magic happens
             divvy_process_station_stats(divvy_api_call_station_status(),
                                         divvy_api_call_station_information())
 
-        information_to_display(information_output_to_display(arrival_information))
+        information_to_display(
+            information_output_to_display(arrival_information))
         tweet_to_display()
 
         REFRESH_DISPLAY = time.monotonic()
